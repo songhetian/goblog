c6e555d252ad17416c93ff3937d8b27bb4cbb9b9
tianhesong <18500973805@163.com>
2022-11-14 01:24:38 +0800

    自动重载

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
 		fmt.Fprintf(w, "<h1>hello,golang2</h1>")
 	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "<h1>请求的地址是:"+r.URL.Path+"</h1>")
+		fmt.Fprint(w, "<h1>请求的地址1是:"+r.URL.Path+"</h1>")
 	} else {
 		fmt.Fprint(w, "<h1>请求的地址不存在</h1>")
 	}
