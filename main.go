a073625be182b5a9c62f5ad72376fea2b1857ef3
tianhesong <18500973805@163.com>
2022-11-27 15:01:17 +0800

    表单验证

--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"net/url"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/gorilla/mux"
 )
 
 var router = mux.NewRouter()
+
+type ArticlesFormData struct {
+	Title, Body string
+	URL         *url.URL
+	Errors      map[string]string
+}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -40,25 +49,95 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 
 func articlesStoredHandler(w http.ResponseWriter, r *http.Request) {
 
-	err := r.ParseForm()
+	// err := r.ParseForm()
 
-	if err != nil {
-		fmt.Fprintf(w, "请提供正确的数据")
+	// if err != nil {
+	// 	fmt.Fprintf(w, "请提供正确的数据")
 
-		return
+	// 	return
+	// }
+
+	// title := r.PostForm.Get("title")
+
+	// fmt.Fprintf(w, "POST PostForm: %v <br>", r.PostForm)
+	// fmt.Fprintf(w, "POST Form: %v <br>", r.Form)
+	// fmt.Fprintf(w, "title 值: %v <br>", title)
+
+	// fmt.Fprintf(w, "r.Form 中的title值为：%v <br>", r.FormValue("title"))
+	// fmt.Fprintf(w, "r.PostForm 中的title值为：%v <br>", r.PostFormValue("title"))
+
+	// fmt.Fprintf(w, "r.Form 中的test值为：%v <br>", r.FormValue("test"))
+	// fmt.Fprintf(w, "r.PostForm 中的test值为：%v <br>", r.PostFormValue("test"))
+
+	title := r.PostFormValue("title")
+	body := r.PostFormValue("body")
+
+	errors := make(map[string]string)
+
+	//验证标题
+	if title == "" {
+		errors["title"] = "标题不能为空"
+	} else if utf8.RuneCountInString(title) < 3 || utf8.RuneCountInString(title) > 40 {
+		errors["title"] = "标题长度需要介于3-40之间"
 	}
 
-	title := r.PostForm.Get("title")
+	if body == "" {
+		errors["body"] = "内容不能为空"
+	} else if utf8.RuneCountInString(body) < 10 {
+		errors["body"] = "内容长度需大于或等于 10 个字节"
+	}
 
-	fmt.Fprintf(w, "POST PostForm: %v <br>", r.PostForm)
-	fmt.Fprintf(w, "POST Form: %v <br>", r.Form)
-	fmt.Fprintf(w, "title 值: %v <br>", title)
+	if len(errors) == 0 {
+		fmt.Fprint(w, "验证通过!<br>")
+		fmt.Fprintf(w, "title 的值为: %v <br>", title)
+		fmt.Fprintf(w, "title 的长度为: %v <br>", utf8.RuneCountInString(title))
+		fmt.Fprintf(w, "body 的值为: %v <br>", body)
+		fmt.Fprintf(w, "body 的长度为: %v <br>", utf8.RuneCountInString(body))
+	} else {
+		html := `
+				<!DOCTYPE html>
+				<html lang="en">
+				<head>
+					<title>创建文章 —— 我的技术博客</title>
+					<style type="text/css">.error {color: red;}</style>
+				</head>
+				<body>
+					<form action="{{ .URL }}" method="post">
+						<p><input type="text" name="title" value="{{ .Title }}"></p>
+						{{ with .Errors.title }}
+						<p class="error">{{ . }}</p>
+						{{ end }}
+						<p><textarea name="body" cols="30" rows="10">{{ .Body }}</textarea></p>
+						{{ with .Errors.body }}
+						<p class="error">{{ . }}</p>
+						{{ end }}
+						<p><button type="submit">提交</button></p>
+					</form>
+				</body>
+				</html>
+				`
 
-	fmt.Fprintf(w, "r.Form 中的title值为：%v <br>", r.FormValue("title"))
-	fmt.Fprintf(w, "r.PostForm 中的title值为：%v <br>", r.PostFormValue("title"))
+		storeUrl, _ := router.Get("articles.store").URL()
 
-	fmt.Fprintf(w, "r.Form 中的test值为：%v <br>", r.FormValue("test"))
-	fmt.Fprintf(w, "r.PostForm 中的test值为：%v <br>", r.PostFormValue("test"))
+		data := ArticlesFormData{
+			Title:  title,
+			Body:   body,
+			URL:    storeUrl,
+			Errors: errors,
+		}
+
+		tmpl, err := template.New("create-form").Parse(html)
+
+		if err != nil {
+			panic(err)
+		}
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			panic(err)
+		}
+
+	}
 
 }
 
